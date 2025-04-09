1df0f3e3bab4d32555d5d4a3409d65f78cd164d8
azrasugec <azrasugec@gmail.com>
2025-04-09 11:24:32 +0300

    test

--- import_nodes.py
+++ import_nodes.py
@@ -1,7 +1,23 @@
+
 class Node:
     def __init__(self, id, latitude, longitude, name):
         self.id = id
         self.latitude = latitude
         self.longitude = longitude
         self.name = name
+
+import csv
+
+def import_csv(filename):
+    nodes = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # başlığı atla
+        for row in reader:
+            id = row[0]
+            latitude = row[1]
+            longitude = row[2]
+            name = row[3]
+            nodes.append(Node(id, latitude, longitude, name))
+    return nodes
 
